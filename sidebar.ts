b4a88aa9dbf526fb237ba1b4149851d588568100
Sang Nguyen <sangnguyenplus@gmail.com>
2023-08-16 14:54:46 +0700

    Update docs

--- sidebar.ts
+++ sidebar.ts
@@ -19,22 +19,22 @@ export default [
         ],
     },
     {
-        text: 'Available plugins',
-        items: [
-            { text: 'Backup', link: '/flex-home/plugin-backup' },
-            { text: 'Language', link: '/flex-home/plugin-language' },
-        ],
-    },
-    {
         text: 'Usage',
         items: [
-            { text: 'Setup email', link: '/flex-home/usage-email' },
+            { text: 'Rename theme', link: '/flex-home/theme-rename' },
+            { text: 'Custom CSS/JS', link: '/flex-home/usage-custom-css-js' },
+            { text: 'Translation', link: '/flex-home/usage-translation' },
+            { text: 'Multi-language', link: '/flex-home/usage-multi-language' },
+            { text: 'Currencies', link: '/flex-home/usage-currencies' },
+            { text: 'Location', link: '/flex-home/usage-location' },
+            { text: 'Google Analytics', link: '/flex-home/usage-analytics' },
             { text: 'Media - Setup Amazon S3', link: '/flex-home/usage-media-s3' },
             { text: 'Media - Setup BunnyCDN', link: '/flex-home/usage-media-bunnycdn' },
             { text: 'Media - Setup Wasabi', link: '/flex-home/usage-media-wasabi' },
+            { text: 'Setup email', link: '/flex-home/usage-email' },
+            { text: 'Backup', link: '/flex-home/plugin-backup' },
+
             { text: 'Setup social login', link: '/flex-home/usage-social-login' },
-            { text: 'Analytics', link: '/flex-home/usage-analytics' },
-            { text: 'Rename theme', link: '/flex-home/theme-rename' },
         ],
     },
 ] satisfies DefaultTheme.SidebarItem[]
